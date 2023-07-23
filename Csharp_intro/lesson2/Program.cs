ced1292d284f7ff691c70cfd8ebc012fbce8d1a6
reggyshicky <reginahshikanda@gmail.com>
2023-07-23 16:19:22 +0300

    Program.cs

---
+++ Csharp_intro/lesson2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+void accept_input(string answer)
+{
+	Console.WriteLine(answer);
+}
+
+void ask_input()
+{
+	Console.WriteLine("Please give us your age");
+	string input = Console.ReadLine();
+	accept_input(input);
+}
+
+ask_input();
