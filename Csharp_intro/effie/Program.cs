0465898a2022cb0e1f1e1fcadae4e6cca6fa8585
reggyshicky <reginahshikanda@gmail.com>
2023-07-25 21:25:43 +0300

    classes of C#

---
+++ Csharp_intro/effie/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+string name = "effie";
+Console.WriteLine("Goodmornng everyone");
+Console.WriteLine("Hello Ven");
+Console.WriteLine(name);
+
+Console.WriteLine("What is your age?");
+
+string age = Console.ReadLine();
+int age_int = int.Parse(age);
+
+//string input = Console.ReadLine();
+//int age = int.Parse(input)
+
+Console.WriteLine($"Ven's age is {age_int}");
+
---
+++ Csharp_intro/lesson3/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "reginah";
+string name2 = "shicky";
+Console.WriteLine("Hello " + name);
+
+//string interpolation
+Console.WriteLine($"Hello {name} and {name2}");
+Console.WriteLine($"The name {name} has {name.Length} and the name {name2} has {name2.Length}");
+
+
+//Triming whitespaces using TrimStart(). TrimEnd() and Trim()
+string greeting = "    Hello Reginah    ";
+Console.WriteLine(greeting.TrimStart());
+Console.WriteLine(greeting.TrimEnd());
+Console.WriteLine(greeting.Trim());
+
+//use of Replace in string
+string sayHello = "Hello World";
+Console.WriteLine(sayHello);
+
+sayHello = sayHello.Replace("Hello", "Habari");
+Console.WriteLine(sayHello);
