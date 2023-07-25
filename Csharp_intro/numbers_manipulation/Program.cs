acbbb05b5e300d55e2e23a60a2720682d122ead7
reggyshicky <reginahshikanda@gmail.com>
2023-07-26 01:54:10 +0300

    numbers in C#

---
+++ Csharp_intro/numbers_manipulation/Program.cs
@@ -0,0 +1,18 @@
+﻿//maximum and minimum interger value in  C#
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The maximum value is {max} and the minimum value is {min}");
+//max and min value of a double in C#
+double max_ = double.MaxValue;
+double min_ = double.MinValue;
+Console.WriteLine($"Double has a max value of {max_} and min value of {min_}");
+double result = 1.0 / 3.0;
+Console.WriteLine(result);
+
+//decimal type, has a smaller range but a higher precision that the double
+decimal max_1 = decimal.MaxValue;
+decimal min_2 = decimal.MinValue;
+Console.WriteLine($"The max value for a dec is {max_1} and the min value  for a dec is {min_2}");
+
+
+
